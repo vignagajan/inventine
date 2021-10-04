c989c479acefc1fdabd10a76d1d308ad28633821
vithiyasahar <vithiyasaharvigneswaran@gmail.com>
2021-10-05 01:19:14 +0530

    Competition Dao && Model

--- src/test/java/com/inventine/model/CompetitionTest.java
+++ src/test/java/com/inventine/model/CompetitionTest.java
@@ -18,17 +18,23 @@ class CompetitionTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/model/Competition.csv", numLinesToSkip = 1)
-    void getterSetter(String competition_id, Timestamp starting_at, Timestamp ending_at, int prize_money, String type, String rules) {
+    void getterSetter(String competition_Id,String organization_Id,String support_Team_Id,String project_Id, Timestamp created_At, Timestamp ending_at, int prize_money, String type, String rules) {
 
-        this.competition.setCompetitionId(competition_id);
-        this.competition.setStartingAt(starting_at);
+        this.competition.setCompetitionId(competition_Id);
+        this.competition.setOrganizationId(organization_Id);
+        this.competition.setProjectId(project_Id);
+        this.competition.setSupportTeamId(support_Team_Id);
+        this.competition.setCreatedAt(created_At);
         this.competition.setEndingAt(ending_at);
         this.competition.setPrizeMoney(prize_money);
         this.competition.setType(type);
         this.competition.setRules(rules);
 
-        assertEquals(competition_id, this.competition.getCompetitionId());
-        assertEquals(starting_at, this.competition.getStartingAt());
+        assertEquals(competition_Id, this.competition.getCompetitionId());
+        assertEquals(organization_Id, this.competition.getOrganizationId());
+        assertEquals(support_Team_Id,this.competition.getSupportTeamId());
+        assertEquals(project_Id, this.competition.getProjectId());
+        assertEquals(created_At, this.competition.getCreatedAt());
         assertEquals(ending_at, this.competition.getEndingAt());
         assertEquals(prize_money, this.competition.getPrizeMoney());
         assertEquals(type, this.competition.getType());
