e430c9370a58453633661540491d38cdeb59276e
unknown <vithiyasaharvigneswaran@gmail.com>
2021-12-26 16:44:49 +0530

    university backend part finished

--- src/main/java/com/inventine/model/University.java
+++ src/main/java/com/inventine/model/University.java
@@ -2,7 +2,10 @@ package com.inventine.model;
 
 import com.inventine.util.DataValidator;
 
-public class University extends Organization{
+public class University extends Organization {
+
+    private String universityId;
+    private String organizationId;
     private String email;
     private DataValidator validator = new DataValidator();
 
@@ -13,14 +16,45 @@ public class University extends Organization{
     public boolean setEmail(String email) {
         this.validator.setTxt(email);
         this.validator.setMaxLength(50);
-        this.validator.setMinLength(6);
 
-        if(this.validator.isEmail()){
+
+        if (this.validator.isEmail()) {
             this.email = email;
             return true;
 
         }
 
+        return false;
+    }
+
+    public String getUniversityId() {
+        return universityId;
+    }
+
+    public boolean setUniversityId(String universityId) {
+        this.validator.setTxt(universityId);
+        this.validator.setMaxLength(6);
+        if (this.validator.isNumber()) {
+            this.universityId = universityId;
+            return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public String getOrganizationId() {
+        return organizationId;
+    }
+
+    @Override
+    public boolean setOrganizationId(String organizationId) {
+        this.validator.setTxt(organizationId);
+        this.validator.setMaxLength(6);
+        if (this.validator.isNumber()) {
+            this.organizationId = organizationId;
+            return true;
+        }
         return false;
     }
 }
