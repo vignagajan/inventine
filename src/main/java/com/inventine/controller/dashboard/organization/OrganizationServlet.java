3b7f68fe70186d87d37607e00214193055d57a60
vithiyasahar <vithiyasaharvigneswaran@gmail.com>
2022-03-20 11:58:18 +0530

    Organization Servelet

--- src/main/java/com/inventine/controller/dashboard/organization/OrganizationServlet.java
+++ src/main/java/com/inventine/controller/dashboard/organization/OrganizationServlet.java
@@ -4,7 +4,7 @@ import com.inventine.dao.CompetitionDaoImplementation;
 import com.inventine.dao.OrganizationDaoImplementation;
 import com.inventine.model.Organization;
 import com.inventine.model.Organization;
-import com.inventine.model.Project;
+import com.inventine.model.Organization;
 import com.inventine.util.DotEnv;
 import org.json.simple.JSONObject;
 
@@ -19,91 +19,98 @@ import java.util.List;
 public class OrganizationServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        HttpSession session = request.getSession();
-//
-//        if (session.getAttribute("role") == null){
-//            session.setAttribute("role", 'A' );
-//        }
-//
-       OrganizationDaoImplementation organizationDao = new OrganizationDaoImplementation();
-//
-//
-//        String get_condition = "";
-////        String totalq= null;
-//        char role = (char)request.getSession().getAttribute("role");
-//
-//        if (role == 'A' || role == 'S'){
-//            get_condition = "";
-//        }
-//
-//        if (role == 'C'){
-//            get_condition = String.format("creatorid=%s", session.getAttribute("userid"));
-//        }
-//
-//
-//
-//
-//       List<Organization> organizations = organizationDao.getOrganizations(get_condition);
-//
-//        int card1_count = 0;
-//        int card2_count = 0;
-//        int card3_count = 0;
-//        int card4_count = 0;
-//
-//        for (Organization organization : organizations) {
-//
-//            if(Character.compare(organization.getStatus(),'A') == 0){
-//                card1_count += 1;
-//            }
-//            if(Character.compare(organization.getStatus(),'B') == 0){
-//                card2_count += 1;
-//            }
-//            if(Character.compare(organization.getStatus(),'D') == 0){
-//                card3_count += 1;
-//            }
-//
-//
-//        }
-//
-//        card4_count = card1_count+card2_count+card3_count;
-//
-//        // Add card labels
-//        request.setAttribute("card1_label","Active");
-//        request.setAttribute("card2_label","Blocked");
-//        request.setAttribute("card3_label","Deleted");
-//        request.setAttribute("card4_label","Total");
-//
-//        // Add card values
-//        request.setAttribute("card1_count",card1_count);
-//        request.setAttribute("card2_count",card2_count);
-//        request.setAttribute("card3_count",card3_count);
-//        request.setAttribute("card4_count",card4_count);
-//
-//        // Add card colors
-//        request.setAttribute("card1_color","#03D815");
-//        request.setAttribute("card2_color","#FFC400");
-//        request.setAttribute("card3_color","#FFC400");
-//        request.setAttribute("card4_color","#FFC400");
-//
-//        // Add card icons
-//        request.setAttribute("card1_icon","fa-lightbulb");
-//        request.setAttribute("card2_icon","fa-lightbulb");
-//        request.setAttribute("card3_icon","fa-lightbulb");
-//        request.setAttribute("card4_icon","fa-lightbulb");
 
 
-        List<Organization> organizations = organizationDao.getOrganizations("");
-        request.setAttribute("organizations",organizations);
+        HttpSession session = request.getSession();
 
-        request.setAttribute("host_url", DotEnv.load().get("HOST_URL"));
-        request.setAttribute("title","Organization");
-        response.setContentType("text/html");
-        request.getRequestDispatcher("/WEB-INF/dashboard/organization/index.jsp").forward(request, response);
-    }
+        if (session.getAttribute("role") == null){
+            session.setAttribute("role", 'A' );
+        }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        OrganizationDaoImplementation organizationDao = new OrganizationDaoImplementation();
 
 
-    }
-}
+
+        String get_condition = "";
+//        String totalq= null;
+        char role = (char)request.getSession().getAttribute("role");
+
+        if (role == 'A' || role == 'F' || role == 'S'){
+            get_condition = "";
+        }
+
+        if (role == 'C'){
+            get_condition = String.format("organizationid=%s", session.getAttribute("userid"));
+        }
+
+
+
+
+        List<Organization> organizations = organizationDao.getOrganizations(get_condition);
+
+        int card1_count = 0;
+        int card2_count = 0;
+        int card3_count = 0;
+        int card4_count = 0;
+
+        for (Organization organization : organizations) {
+
+
+            if (Character.compare(organization.getStatus(), 'D') == 0) {
+                card4_count += 1;}
+                if (Character.compare(organization.getOrgType(), 'S') == 0) {
+                    card2_count += 1;}
+                    if (Character.compare(organization.getOrgType(), 'U') == 0) {
+                        card3_count += 1;}
+                        if (Character.compare(organization.getOrgType(), 'P') == 0) {
+                            card1_count += 1;
+                        }
+
+
+                    }
+
+                    card4_count = card1_count + card2_count + card3_count;
+
+                    // Add card labels
+                    request.setAttribute("card1_label", "P.Organizations");
+                    request.setAttribute("card2_label", "Schools");
+                    request.setAttribute("card3_label", "Universities");
+                    request.setAttribute("card4_label", "Deleted");
+
+                    // Add card values
+                    request.setAttribute("card1_count", card1_count);
+                    request.setAttribute("card2_count", card2_count);
+                    request.setAttribute("card3_count", card3_count);
+                    request.setAttribute("card4_count", card4_count);
+
+                    // Add card colors
+                    request.setAttribute("card1_color", "#03D815");
+                    request.setAttribute("card2_color", "#FFC400");
+                    request.setAttribute("card3_color", "#0097E6");
+                    request.setAttribute("card4_color", "#FF0000");
+
+                    // Add card icons
+                    request.setAttribute("card1_icon", "far fa-building");
+                    request.setAttribute("card2_icon", "far fa-building");
+                    request.setAttribute("card3_icon", "far fa-building");
+                    request.setAttribute("card4_icon", "far fa-building");
+
+
+                    request.setAttribute("organizations", organizations);
+
+                    request.setAttribute("host_url", DotEnv.load().get("HOST_URL"));
+                    request.setAttribute("title", "Organization");
+                    response.setContentType("text/html");
+                    request.getRequestDispatcher("/WEB-INF/dashboard/organization/index.jsp").forward(request, response);
+                }
+
+                @Override
+                protected void doPost (HttpServletRequest request, HttpServletResponse response) throws
+                ServletException, IOException {
+
+
+                }
+
+
+
+        }
