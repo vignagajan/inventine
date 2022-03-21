525805b25f592600a2d840fe8fab79268597f4e2
amiladimantha <amiladimantha123@gmail.com>
2022-03-21 12:13:35 +0530

    payment and project servlet updates

--- src/main/java/com/inventine/controller/dashboard/payment/PaymentServlet.java
+++ src/main/java/com/inventine/controller/dashboard/payment/PaymentServlet.java
@@ -32,23 +32,117 @@ public class PaymentServlet extends HttpServlet {
         PaymentDaoImplementation paymentDao = new PaymentDaoImplementation();
         ProjectDaoImplementation projectDao = new ProjectDaoImplementation();
 
-        List<Payment> payments = paymentDao.getPayments("");
+        String card1_condition= null;
+        String card2_condition= null;
+        String card3_condition= null;
+        String card4_condition= null;
+        String get_condition = "";
+        String totalq= null;
 
-
-        String creatorsq= null;
-        String projectsq= null;
-        String investorsq= null;
-        String transactionsq= null;
-//        String totalq= null;
         char role = (char)request.getSession().getAttribute("role");
 
         if (role == 'A' || role == 'F' || role == 'S'){
-            creatorsq = "select count(DISTINCT creatorid) from project where projectid=(select distinct projectid from payment)";
-            projectsq = "select count(DISTINCT projectid) from payment";
-            investorsq = "select count(DISTINCT investorid) from payment";
-            transactionsq = "select sum(amount/(1000)) from payment";
+            card1_condition = "select count from payment where status = 'A'";
+            card2_condition = "select count from project where status = 'B'";
+            card3_condition = "select count from project where status = 'D'";
+            card4_condition = "select count(DISTINCT projectid) from project";
+            get_condition = "";
         }
 
+        if (role == 'C'){
+            card2_condition = "select count from project where status='A' && creatorid=%s";
+            card3_condition = "select count from project where status='B' && creatorid=%s";
+            card4_condition = "select count from project where status='D' && creatorid=%s";
+            totalq = "select sum(amount/(1000)) from payment where projectid=(select projectid from project where creatorid=%s)";
+        }
+
+        if (role == 'I'){
+            card1_condition = "select count(DISTINCT projectid) from project";
+            card2_condition = "(select count from payment where projectid=(select projectid from project where financialstatus='C') && investorid=%s)";
+            card3_condition = "select count(DISTINCT investorid) from payment";
+            card4_condition = "select sum(amount/(1000)) from payment where investorid=%s";
+        }
+
+//        String creators = String.format("select count(investorid) from payment where investorid=%s;",investorId);
+//        String projects = String.format("select count(investorid) from payment where investorid=%s;",investorId);
+//        String meetings = String.format("select count(investorid) from acceptmeeting where investorid=%s;",investorId);
+//        String transactions = String.format("select sum(amount/(1000)) from payment where investorid=%s;",investorId);
+
+        List<Payment> payments = paymentDao.getPayments(get_condition);
+        List<Project> projects = projectDao.getProjects(get_condition);
+
+
+        int card1_count = 0;
+        int card2_count = 0;
+        int card3_count = 0;
+        int card4_count = 0;
+
+//        for (Project project : projects) {
+//
+//            if(Character.compare(project.getStatus(),'A') == 0){
+//                card1_count += 1;
+//            }
+//            if(Character.compare(project.getStatus(),'B') == 0){
+//                card2_count += 1;
+//            }
+//            if(Character.compare(project.getStatus(),'D') == 0){
+//                card3_count += 1;
+//            }
+//
+//
+//        }
+
+//        card4_count = card1_count+card2_count+card3_count;
+
+        // Add card labels
+        if (role == 'A' || role == 'F' || role == 'S' || role == 'C' || role == 'I') {
+            request.setAttribute("card1_label", "This Month");
+            request.setAttribute("card2_label", "This Week");
+            request.setAttribute("card3_label", "Transactions");
+            request.setAttribute("card4_label", "Total Funds");
+        }
+
+        // Add card values
+        request.setAttribute("card1_count",card1_count);
+        request.setAttribute("card2_count",card2_count);
+        request.setAttribute("card3_count",card3_count);
+        request.setAttribute("card4_count",card4_count);
+
+        // Add card colors
+        request.setAttribute("card1_color","#006EFF");
+        request.setAttribute("card2_color","#FFC400");
+        request.setAttribute("card3_color","#FF004C");
+        request.setAttribute("card4_color","#03D815");
+
+        // Add card icons
+        request.setAttribute("card1_icon","fa-lightbulb");
+        request.setAttribute("card2_icon","fa-lightbulb");
+        request.setAttribute("card3_icon","fa-lightbulb");
+        request.setAttribute("card4_icon","fa-lightbulb");
+
+        // Add table data
+//        request.setAttribute("payments",payments);
+//        request.setAttribute("projects",projects);
+//
+//        request.setAttribute("title","Project");
+//        request.getRequestDispatcher("/WEB-INF/dashboard/project/index.jsp").forward(request, response);
+//        List<Payment> payments = paymentDao.getPayments("");
+//
+//
+//        String creatorsq= null;
+//        String projectsq= null;
+//        String investorsq= null;
+//        String transactionsq= null;
+//        String totalq= null;
+//        char role = (char)request.getSession().getAttribute("role");
+//
+//        if (role == 'A' || role == 'F' || role == 'S'){
+//            creatorsq = "select count(DISTINCT creatorid) from project where projectid=(select distinct projectid from payment)";
+//            projectsq = "select count(DISTINCT projectid) from payment";
+//            investorsq = "select count(DISTINCT investorid) from payment";
+//            transactionsq = "select sum(amount/(1000)) from payment";
+//        }
+//
 //        if (role == 'C'){
 //            projectsq = "select count(DISTINCT projectid) from payment";
 //            investorsq = "select count(DISTINCT investorid) from payment";
@@ -62,31 +156,31 @@ public class PaymentServlet extends HttpServlet {
 //            transactionsq = "select sum(amount/(1000)) from payment";
 //            totalq = "select sum(amount/(1000)) from payment where projectid=(select projectid from project where creatorid=%s)";
 //        }
-
+//
 //        String creators = String.format("select count(investorid) from payment where investorid=%s;",investorId);
 //        String projects = String.format("select count(investorid) from payment where investorid=%s;",investorId);
 //        String meetings = String.format("select count(investorid) from acceptmeeting where investorid=%s;",investorId);
 //        String transactions = String.format("select sum(amount/(1000)) from payment where investorid=%s;",investorId);
-
-
-
-
-        if(investorId.isEmpty()){
-            System.out.println("Investor ID is empty");
-        }
-
-        ResultSet rsc = paymentDao.executeQuery(creatorsq);
-        ResultSet rsp = paymentDao.executeQuery(projectsq);
-        ResultSet rsm = paymentDao.executeQuery(investorsq);
-        ResultSet rst = paymentDao.executeQuery(transactionsq);
-        try {
-            request.setAttribute("creatorsq", rsc.getInt("count"));
-            request.setAttribute("projectsq", rsp.getInt("count"));
-            request.setAttribute("investorsq", rsm.getInt("count"));
-            request.setAttribute("transactionsq", rst.getInt("sum"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+//
+//
+//
+//
+//        if(investorId.isEmpty()){
+//            System.out.println("Investor ID is empty");
+//        }
+//
+//        ResultSet rsc = paymentDao.executeQuery(creatorsq);
+//        ResultSet rsp = paymentDao.executeQuery(projectsq);
+//        ResultSet rsm = paymentDao.executeQuery(investorsq);
+//        ResultSet rst = paymentDao.executeQuery(transactionsq);
+//        try {
+//            request.setAttribute("creatorsq", rsc.getInt("count"));
+//            request.setAttribute("projectsq", rsp.getInt("count"));
+//            request.setAttribute("investorsq", rsm.getInt("count"));
+//            request.setAttribute("transactionsq", rst.getInt("sum"));
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
 
 
         request.setAttribute("payments",payments);
